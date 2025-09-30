563050554e0b21f314bad976bf0343c2366d7b1f
evoj1 <evoj1cehovich@gmail.com>
2025-09-30 22:03:24 +0700

    Исходный код

--- PIS2/Program.cs
+++ PIS2/Program.cs
@@ -10,6 +10,65 @@ namespace PIS2
     {
         static void Main(string[] args)
         {
+            string input = "2025.09.09  \"23-03\"  \"Губерниев Иван\"";
+            Console.WriteLine(ParseString(input));
+        }
+
+        static Lesson ParseString(string input)
+        {
+            string[] array = input.Split(new char[] { ' ', '"' }, StringSplitOptions.RemoveEmptyEntries);
+            string date = array[0];
+            string cabinet = array[1];
+            string teacherName = ParseName(input);
+
+            DateTime dateTime = DateTime.ParseExact(date, "yyyy.MM.dd", null);
+
+            return new Lesson(dateTime, cabinet, teacherName);
+        }
+
+        static string ParseName(string input)
+        {
+            int quoteCount = 0;
+            StringBuilder nameBuilder = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                if (c == '"')
+                {
+                    quoteCount++;
+                    continue;
+                }
+
+                if (quoteCount == 3)
+                {
+                    nameBuilder.Append(c);
+                }
+                else if (quoteCount > 3)
+                {
+                    break;
+                }
+            }
+            return nameBuilder.ToString();
+        }
+        public class Lesson
+        {
+            public DateTime DateTime { get; set; }
+            public string Cabinet { get; set; }
+            public string TeacherName { get; set; }
+
+            public Lesson(DateTime dateTime, string cabinet, string teacherName)
+            {
+                DateTime = dateTime;
+                Cabinet = cabinet;
+                TeacherName = teacherName;
+            }
+
+            public override string ToString()
+            {
+                return $"{DateTime:yyyy.MM.dd}\n" +
+                    $"{Cabinet}\n" +
+                    $"{TeacherName}\n";
+            }
         }
     }
 }
