120d1fdc0ae1039f2838827fd2ec298f6a26464f
evoj1 <evoj1cehovich@gmail.com>
2025-10-01 16:00:20 +0700

    Добавление свойства CountLessons в OnlineLesson

--- PIS2/OnlineLesson.cs
+++ PIS2/OnlineLesson.cs
@@ -12,19 +12,22 @@ namespace PIS2
         public string Platform { get; set; }
         public string TeacherName { get; set; }
         public string MeetingLink { get; set; }
-        public OnlineLesson(DateTime dateTime, string platform, string teacherName, string meetingLink)
+        public int CountLessons { get; set; }
+        public OnlineLesson(DateTime dateTime, string platform, string teacherName, string meetingLink, int countLessons)
         {
             DateTime = dateTime;
             Platform = platform;
             TeacherName = teacherName;
             MeetingLink = meetingLink;
+            CountLessons = countLessons;
         }
         public override string ToString()
         {
             return $"Дата: {DateTime}\n" +
                 $"Платформа: {Platform}\n" +
                 $"Имя преподавателя: {TeacherName}\n" +
-                $"Ссылка: {MeetingLink}";
+                $"Ссылка: {MeetingLink}\n" +
+                $"Количество уроков: {CountLessons}";
         }
     }
 }
